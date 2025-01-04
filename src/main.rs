da3779b0729ed1e8493b810517aedf7081bf1ddb
iosh <ioshx@foxmail.com>
2025-01-04 22:38:42 +0800

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
 mod address;
 
-use std::thread;
 use address::PrivateKeyAccount;
+use clap::Parser;
 use num_cpus;
+use std::thread;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Maximum number of times to generate addresses.
+    #[arg(short, long)]
+    max: Option<u64>,
+
+    /// Maximum number of results to return.
+    #[arg(short, long)]
+    limit: Option<u64>,
+
+    /// Maximum number of concurrent threads to use.
+    #[arg(short, long)]
+    threads: Option<u16>,
+
+    /// Strings that the address must contain (supports multiple strings, separated by commas).
+    #[arg(short, long)]
+    contains: Vec<String>,
+
+    /// Prefix that the address must match.
+    #[arg(short, long)]
+    prefix: Option<String>,
+
+    /// Suffix that the address must match.
+    #[arg(short, long)]
+    suffix: Option<String>,
+
+    /// Regular expression that the address must match.
+    #[arg(short, long)]
+    regex: Option<String>,
+}
 
 fn main() {
+    let args = Args::parse();
     let num_threads = num_cpus::get();
     let mut handles = vec![];
 
