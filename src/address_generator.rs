58452f98336f162ecae0706d56b7c824eeeb88e0
iosh <ioshx@foxmail.com>
2025-01-14 21:03:38 +0800

    chore: update address generator

--- src/address_generator.rs
+++ src/address_generator.rs
@@ -1,7 +1,7 @@
 use bip32::DerivationPath;
 
 use crate::{
-    address::{MnemonicAccount, PrivateKeyAccount},
+    address::{Address, MnemonicAccount, PrivateKeyAccount},
     validator::AddressValidator,
 };
 
@@ -21,59 +21,52 @@ impl AddressGenerator {
     ) -> AddressGenerator {
         AddressGenerator {
             from_private_key,
-            derivation_path: derivation_path
-                .parse()
-                .map_err(|e| format!("Invalid derivation path: {}", e))
-                .unwrap(),
+            derivation_path: derivation_path.parse().expect("Invalid derivation path"),
             validator,
             address_format,
         }
     }
-
     pub fn new_random_address(&self, net: u32) -> Option<(String, String)> {
-        let (address, secret) = if self.from_private_key {
-            let private_key_account = PrivateKeyAccount::from_random_private_key();
-            let address = match self.address_format {
-                AddressFormat::HEX => private_key_account.address.hex_address(),
-                AddressFormat::BASE32 => private_key_account.address.base32_address(net),
+        let (address, secret) = self.generate_address_and_secret(net);
+
+        let address_to_validate = match self.address_format {
+            AddressFormat::HEX => &address,
+            AddressFormat::BASE32 => address.split(':').nth(1).expect("Invalid base32 address"),
+        };
+
+        if self.validator.validate(address_to_validate) {
+            let formatted_address = match self.address_format {
+                AddressFormat::HEX => format!("0x{}", address),
+                AddressFormat::BASE32 => address,
             };
+            return Some((formatted_address, secret));
+        };
+        None
+    }
+
+    fn generate_address_and_secret(&self, net: u32) -> (String, String) {
+        if self.from_private_key {
+            let private_key_account = PrivateKeyAccount::from_random_private_key();
+            let address = self.format_address(&private_key_account.address, net);
             (
                 address,
                 private_key_account.secret_key.display_secret().to_string(),
             )
         } else {
             let mnemonic_account = MnemonicAccount::from_random_mnemonic(12, &self.derivation_path);
-
-            let address = match self.address_format {
-                AddressFormat::HEX => mnemonic_account.address.hex_address(),
-                AddressFormat::BASE32 => mnemonic_account.address.base32_address(net),
-            };
+            let address = self.format_address(&mnemonic_account.address, net);
             (address, mnemonic_account.mnemonic.to_string())
-        };
-
-        let match_address = match self.address_format {
-            AddressFormat::HEX => &address,
-            AddressFormat::BASE32 => {
-                let parts: Vec<&str> = address.split(':').collect();
-                if parts.len() > 1 {
-                    parts[1]
-                } else {
-                    panic!("Invalid address format")
-                }
-            }
-        };
-
-        if self.validator.validate(match_address) {
-            return match self.address_format {
-                AddressFormat::HEX => Some((format!("0x{}", address), secret)),
-                AddressFormat::BASE32 => Some((address, secret)),
-            };
-        };
-        None
+        }
+    }
+    fn format_address(&self, address: &Address, net: u32) -> String {
+        match self.address_format {
+            AddressFormat::HEX => address.hex_address(),
+            AddressFormat::BASE32 => address.base32_address(net),
+        }
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum AddressFormat {
     HEX,
     BASE32,
@@ -81,10 +74,9 @@ pub enum AddressFormat {
 
 impl From<&str> for AddressFormat {
     fn from(s: &str) -> Self {
-        match s {
-            "HEX" => AddressFormat::HEX,
+        match s.to_uppercase().as_str() {
             "BASE32" => AddressFormat::BASE32,
-            _ => panic!("Invalid address format"),
+            _ => AddressFormat::HEX,
         }
     }
 }
